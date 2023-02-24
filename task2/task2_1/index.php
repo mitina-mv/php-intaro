8bafe76c9b4990fa6cbbb198dcc458951a957212
lolipopmari <mit.maria2019@yandex.ru>
2023-02-25 00:53:55 +0300

    добавлен файл с общими стилями, чтобы не плодить лишние файлы. добалвена таблица вывода результатов

--- task2/task2_1/index.php
+++ task2/task2_1/index.php
@@ -8,10 +8,49 @@ $outerContent = [];
 
 foreach($innerContent as $str)
 {
-    $newStr = preg_replace_callback("/['][\d]+[']/", function($matches){
-        $num = trim($matches[0], "'");
-        return "'" . (int)$num * 2 . "'";
-    }, $str);
-
-    echo "$str  ->  $newStr <br>";
+    $outerContent[] = preg_replace_callback(
+        "/['][\d]+[']/", 
+        function($matches)
+        {
+            $num = trim($matches[0], "'");
+            return "'" . (int)$num * 2 . "'";
+        }, 
+        $str
+    );
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TASK 2 - 1</title>
+    <link rel="stylesheet" href="/common/common-styles.css">
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>№</th>
+                <th>Входная строка</th>
+                <th>Выходная строка</th>
+            </tr>
+        </thead>
+
+        <tbody>            
+            <?php foreach($outerContent as $key => $str): ?>
+                <tr>
+                    <td><?=$key + 1?></td>
+                    <td><?=$innerContent[$key]?></td>
+                    <td><?=$str?></td>
+                </tr>
+            <?php endforeach;?>
+            <tr></tr>
+        </tbody>
+    </table>
+</body>
+</html>
+
