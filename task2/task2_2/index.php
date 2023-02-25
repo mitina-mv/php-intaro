aa889c24abbb8eaac1101e6f23d9c2ecaf1e2a4a
lolipopmari <mit.maria2019@yandex.ru>
2023-02-25 15:13:00 +0300

    добавлены комментации по коду php для всех заданий

--- task2/task2_2/index.php
+++ task2/task2_2/index.php
@@ -1,14 +1,18 @@
 <?php
 declare(strict_types = 1);
 
+// подключение общих функций
 require_once $_SERVER['DOCUMENT_ROOT'] . "/common/common-function.php";
 
+// файл-источнк строк
 $innerContent = file('./test/test.dat');
+// хранилище выходных данных
 $outerContent = [];
 $outerContentRegular = [];
 
 foreach($innerContent as $url)
 {
+    // проверка на соотвествие маске
     if (!preg_match(
         "/http:\/\/asozd\.duma\.gov\.ru\/main\.nsf\/\(Spravka\)\?OpenAgent&RN=[\d-]+&[\d]+/", 
         $url
@@ -20,15 +24,21 @@ foreach($innerContent as $url)
         continue;
     }
 
+    // способ 1 - забираем из url параметр RN и создаем новую строку
+    // соотвествие URL не обязательно, но необходим параметр RN
+    // функция getUrlQuery в файле общих функций
     $paramRN = getUrlQuery($url, 'RN');
     $outerContent[] = "http://sozd.parlament.gov.ru/bill/" . $paramRN;
 
+    // способ 2 - решение прямо по заданию "с помощью регулярного выражения"
+    // сначала меняем начало ссылки до номера приказа
     $newUrl = preg_replace(
         '/http:\/\/asozd\.duma\.gov\.ru\/main\.nsf\/\(Spravka\)\?OpenAgent&RN=/',
         'http://sozd.parlament.gov.ru/bill/',
         $url
     );
 
+    // потом меняем конец ссылки на пустоту, потому что &<целое число> не используется
     $outerContentRegular[] = preg_replace(
         '/&[\d]+/',
         '',
@@ -44,7 +54,7 @@ foreach($innerContent as $url)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TASK 2 - 1</title>
+    <title>TASK 2 - 2</title>
     <link rel="stylesheet" href="/common/common-styles.css">
 </head>
 <body>
