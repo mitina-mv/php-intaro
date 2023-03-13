c838c7c73c7f93b8addc0bc0625aee13e7ac54da
lolipopmari <mit.maria2019@yandex.ru>
2023-03-13 23:30:57 +0300

    работа с классом SimpleXML - не проверено, но планируется изменение

--- task3/task3_2/index.php
+++ task3/task3_2/index.php
@@ -5,14 +5,53 @@ declare(strict_types = 1);
 require './../vendor/autoload.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . "/common/common-function.php";
 
+for($i = 1; $i <= 6; ++$i)
+{
+    // получаем содержимое файлов xml
+    $xmlprod = file_get_contents("./test/00{$i}_products.xml");
+    $xmlres = file_get_contents("./test/00{$i}_result.xml");
+    $xmlsect = file_get_contents("./test/00{$i}_sections.xml");
+
+    $prodSections = [];
+    $prods = [];
+
+    $xmlIterator = new SimpleXMLIterator( $xmlprod );
+    for($xmlIterator->rewind(); $xmlIterator->valid(); $xmlIterator->next()) 
+    {
+        // if($xmlIterator->hasChildren()) 
+        // {
+            $prodSections[(string)$xmlIterator->current()->Ид] = (array)$xmlIterator->current()->Разделы->ИдРаздела;
+            $prods[(string)$xmlIterator->current()->Ид] = (array)$xmlIterator->current();
+        // }
+    }
+    
+    $xmlIterator = new SimpleXMLIterator( $xmlsect );
+    for($xmlIterator->rewind(); $xmlIterator->valid(); $xmlIterator->next()) 
+    {
+        $sectionId = (string)$xmlIterator->current()->Ид;
+        $prodsInSection = $xmlIterator->current()->addChild('Товары');
+        foreach($prodSections as $prodId => $arSectionId)
+        {
+            if(in_array($sectionId, $arSectionId))
+            {
+                $prod = $prodsInSection->addChild('Товар');
+                $prod->addChild('Ид', $prodId);
+                $prod->addChild('Наименование', $prods[$prodId]['Наименование']);
+                $prod->addChild('Артикул', $prods[$prodId]['Артикул']);
+            }
+        }        
+    }
+    echo $xmlsect->asXML();
+}
+
 $xmlstr = file_get_contents('./test/002_products.xml');
 $document = new SimpleXMLElement($xmlstr);
 /* имя корневого элемента */
 // $name = $document->getName();
 // print_r($name);
-echo "<pre>";
-print_r(xmlObjToArr($document));
-echo "</pre>";
+// echo "<pre>";
+// print_r(xmlObjToArr($document));
+// echo "</pre>";
 
 
 // $fabric = (new NavigatorFabric())->setXml($xml);
