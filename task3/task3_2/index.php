c3a61a589488c489a1740462e8f33f22f21b79d6
lolipopmari <mit.maria2019@yandex.ru>
2023-03-13 21:40:30 +0300

    нашла функцию превращения xml в массив, записала в общие функции с пометкой источника

--- task3/task3_2/index.php
+++ task3/task3_2/index.php
@@ -1,2 +1,20 @@
 <?php
 declare(strict_types = 1);
+
+// автозагрузчик от composer
+require './../vendor/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . "/common/common-function.php";
+
+$xmlstr = file_get_contents('./test/002_products.xml');
+$document = new SimpleXMLElement($xmlstr);
+/* имя корневого элемента */
+// $name = $document->getName();
+// print_r($name);
+echo "<pre>";
+print_r(xmlObjToArr($document));
+echo "</pre>";
+
+
+// $fabric = (new NavigatorFabric())->setXml($xml);
+// $converter = $fabric->makeConverter();
+// $arrayRepresentationOfXml = $converter->toArray();
