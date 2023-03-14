13bb5e0f5d3a1f9b8aeeb56b1ad2f9872eb59006
lolipopmari <mit.maria2019@yandex.ru>
2023-03-14 08:45:59 +0300

    заработало, но в результате ожидается раздел "Элементы Каталога", а сейчас в содержимое sections вставляются товары ((

--- task3/task3_2/index.php
+++ task3/task3_2/index.php
@@ -7,98 +7,69 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/common/common-function.php";
 
 for($i = 1; $i <= 6; ++$i)
 {
-    $xmlprod = simplexml_load_file("./test/00{$i}_products.xml");
-    $xmlsect = simplexml_load_file("./test/00{$i}_sections.xml");
-
-    if($xmlsect->Раздел->count() == 1) {
-        $arSections[] = $xmlsect->Раздел;
-    } else {
-        $arSections = ((array)$xmlsect)['Раздел'];
-    }
-    // $arSections;
-
-    $prods = [];
-    if($xmlprod->Товар->count() == 1) {
-        $prods[] = $xmlprod->Товар;
-    } else {
-        $prods = ((array)$xmlprod)['Товар'];
-    }
-
-    foreach($arSections as &$section)
-    {
-        $prodsInSection = $section->addChild('Товары');
-
-        foreach($prods as $prod)
-        {
-            $prodSectId = [];
-            $prodSectId = array_merge($prodSectId, (array)$prod->Разделы->ИдРаздела);
-
-            if(in_array($section->Ид, $prodSectId))
-            {
-                $prodSect = $prodsInSection->addChild('Товар');
-                $prodSect->addChild('Ид', (string)$prod->Ид);
-                $prodSect->addChild('Наименование', (string)$prod->Наименование);
-                $prodSect->addChild('Артикул', (string)$prod->Артикул);
-            }
-        }
-    }
-
-    echo '<pre>';
-    print_r($arSections);
-    echo '</pre>';
-
-    $xmlResult = new SimpleXMLElement('<?xml version="1.0"?><Разделы></Разделы>');
-    arrayToXml($arSections, $xmlResult);
-    $result = $xml_data->asXML('/file/path/name.xml');
-    
-
     // получаем содержимое файлов xml
-    // $xmlprod = file_get_contents("./test/00{$i}_products.xml");
-    // $xmlres = file_get_contents("./test/00{$i}_result.xml");
-    // $xmlsect = file_get_contents("./test/00{$i}_sections.xml");
+    $xmlprod = file_get_contents("./test/00{$i}_products.xml");
+    $xmlres = file_get_contents("./test/00{$i}_result.xml");
+    $xmlsect = file_get_contents("./test/00{$i}_sections.xml");
 
-    // $prodSections = [];
-    // $prods = [];
+    $prodSections = [];
+    $prods = [];
 
-    // $xmlIterator = new SimpleXMLIterator( $xmlprod );
-    // for($xmlIterator->rewind(); $xmlIterator->valid(); $xmlIterator->next()) 
-    // {
-    //     // if($xmlIterator->hasChildren()) 
-    //     // {
-    //         $prodSections[(string)$xmlIterator->current()->Ид] = (array)$xmlIterator->current()->Разделы->ИдРаздела;
-    //         $prods[(string)$xmlIterator->current()->Ид] = (array)$xmlIterator->current();
-    //     // }
-    // }
+    $xmlIterator = new SimpleXMLIterator( $xmlprod );
+    for($xmlIterator->rewind(); $xmlIterator->valid(); $xmlIterator->next()) 
+    {
+        $prodSections[(string)$xmlIterator->current()->Ид] = 
+            (array)$xmlIterator->current()
+                ->Разделы
+                ->ИдРаздела;
+        $prods[(string)$xmlIterator->current()->Ид] = (array)$xmlIterator->current();
+    }
+
+    /* $xmlProgResult = new SimpleXMLElement('<?xml version="1.0"?><Разделы></Разделы>'); */
     
-    // $xmlIterator = new SimpleXMLIterator( $xmlsect );
-    // for($xmlIterator->rewind(); $xmlIterator->valid(); $xmlIterator->next()) 
-    // {
-    //     $sectionId = (string)$xmlIterator->current()->Ид;
-    //     $prodsInSection = $xmlIterator->current()->addChild('Товары');
-    //     foreach($prodSections as $prodId => $arSectionId)
-    //     {
-    //         if(in_array($sectionId, $arSectionId))
-    //         {
-    //             $prod = $prodsInSection->addChild('Товар');
-    //             $prod->addChild('Ид', $prodId);
-    //             $prod->addChild('Наименование', $prods[$prodId]['Наименование']);
-    //             $prod->addChild('Артикул', $prods[$prodId]['Артикул']);
-    //         }
-    //     }        
-    // }
-    // echo $xmlsect->asXML();
+    $xmlIterator = new SimpleXMLIterator( $xmlsect );
+    for($xmlIterator->rewind(); $xmlIterator->valid(); $xmlIterator->next()) 
+    {
+        $sectionId = (string)$xmlIterator->current()->Ид;
+        $prodsInSection = $xmlIterator->current()->addChild('Товары');
+        foreach($prodSections as $prodId => $arSectionId)
+        {
+            if(in_array($sectionId, $arSectionId))
+            {
+                $prod = $prodsInSection->addChild('Товар');
+                $prod->addChild('Ид', $prodId);
+                $prod->addChild('Наименование', $prods[$prodId]['Наименование']);
+                $prod->addChild('Артикул', $prods[$prodId]['Артикул']);
+            }
+        }        
+    }
+
+    $programmResult = $xmlIterator->asXML();
+    echo "<pre>";
+    print_r($programmResult);
+    echo "</pre>";
+    // $tableRows[] = [
+    //     $programmResult == $xmlres,
+    //     $programmResult,
+    //     $xmlres
+    // ];
+
+    // // выводим результаты тестирования 
+    // echo "<h3>Тест #{$i}</h3>";
+    // echo testTableNew(
+    //     ["Статус", "Значение программы", "Значение из файла"],
+    //     $tableRows
+    // );
 }
+?>
 
-// $xmlstr = file_get_contents('./test/002_products.xml');
-// $document = new SimpleXMLElement($xmlstr);
-/* имя корневого элемента */
-// $name = $document->getName();
-// print_r($name);
-// echo "<pre>";
-// print_r(xmlObjToArr($document));
-// echo "</pre>";
-
-
-// $fabric = (new NavigatorFabric())->setXml($xml);
-// $converter = $fabric->makeConverter();
-// $arrayRepresentationOfXml = $converter->toArray();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 4</title>
+    <link rel="stylesheet" href="/common/common-styles.css">
+</head>
+<body>
