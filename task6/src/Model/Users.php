01afba82778131921355d2fb2719eda31a8b86c6
lolipopmari <mit.maria2019@yandex.ru>
2023-04-26 09:06:51 +0300

    feat: догрузила orm pack

--- task6/src/Model/Users.php
+++ task6/src/Model/Users.php
@@ -2,14 +2,27 @@
 
 namespace App\Model;
 
-class Users extends Model
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+ #[ORM\Entity(repositoryClass: ProductRepository::class)]
+class User
 {
-    protected $table = 'users';
-    protected $fillable = [
-        'firstname',
-        'lastname',
-        'patronymic',
-        'email',
-        'password'
-    ];
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $name;
+
+    #[ORM\Column]
+    private int $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    // ... методы геттера и сеттера
 }
