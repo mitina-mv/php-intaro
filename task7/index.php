f86053436079e69ae157579d9097a1454fff4c48
lolipopmari <mit.maria2019@yandex.ru>
2023-05-22 18:03:55 +0300

    feat: оформление как у боженьки, лапки как у картошеньки

--- task7/index.php
+++ task7/index.php
@@ -5,31 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Поиск ближайшего метро</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>    
-    <form action="./getResult.php" method="post">
-        <div class="field">
-            <label for="toun_field">Город</label>
-            <select name="toun" id="toun_field">
-                <option value="Москва" selected>Москва</option>
-                <option value="Санкт-Петербург">Санкт-Петербург</option>
-                <option value="Нижний Новгород">Нижний Новгород</option>
-                <option value="Новосибирск">Новосибирск</option>
-                <option value="Самара">Самара</option>
-                <option value="Казань">Казань</option>
-                <option value="Екатеринбург">Екатеринбург</option>
-            </select>
-        </div>
+    <div class="form-picture">
+        <div class="picture"></div>
 
-        <div class="field">
-            <label for="address_field">Введите свой адрес (без города)</label>
-            <input type="text" name="address" id="address_field">
-        </div>
+        <form action="./getResult.php" method="post">
+            <div class="field">
+                <label for="toun_field">Город</label>
+                <select name="toun" id="toun_field">
+                    <option value="Москва" selected>Москва</option>
+                    <option value="Санкт-Петербург">Санкт-Петербург</option>
+                    <option value="Нижний Новгород">Нижний Новгород</option>
+                    <option value="Новосибирск">Новосибирск</option>
+                    <option value="Самара">Самара</option>
+                    <option value="Казань">Казань</option>
+                    <option value="Екатеринбург">Екатеринбург</option>
+                </select>
+            </div>
 
-        <input type="submit" value="Найти метро!">
+            <div class="field">
+                <label for="address_field">Введите свой адрес (без города)</label>
+                <input type="text" name="address" id="address_field">
+            </div>
 
-        <div class="message-block"></div>
-    </form>
+            <input type="submit" value="Найти метро!">
+
+            <div class="message-block"></div>
+        </form>
+    </div>
 
     <script src="./script.js"></script>
     <script src="./../task4/main.js"></script>
--- task7/script.js
+++ task7/script.js
@@ -2,7 +2,21 @@
 
 window.addEventListener('DOMContentLoaded', function() {
     let form = document.querySelector('form');
-    let touns = ['Москва', 'Санкт-Петербург', 'Нижний Новгород', 'Новосибирск', 'Самара', 'Казань', 'Екатеринбург'];
+
+    function findTountInAddress(mass, block)
+    {
+        let flagReturn = false;        
+        let touns = ['Москва', 'Санкт-Петербург', 'Нижний Новгород', 'Новосибирск', 'Самара', 'Казань', 'Екатеринбург'];
+        
+        mass.forEach(element => {
+            if(touns.indexOf( element ) != -1) {
+                block.innerHTML = '<span class="error">Кажется, вы ввели город в поле для адреса и решили меня запутать! Выберите город чуть выше!</span>'
+                flagReturn = true;
+            }
+        });
+
+        return flagReturn;
+    }
 
     form.addEventListener('submit', function(e) {
         e.preventDefault();
@@ -20,12 +34,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
         // TODO проверка на ввод города в поле адреса
         let arrLocation = address.split(',');
-        arrLocation.forEach(element => {
-            if(touns.indexOf( element ) != -1) {
-                message.innerHTML = '<span class="error">Кажется, вы ввели город в поле для адреса и решили меня запутать! Выберите город чуть выше!</span>'
-                return;
-            }
-        });
+        if(findTountInAddress(arrLocation, message)) return;
+        arrLocation = address.split(' ');
+        if(findTountInAddress(arrLocation, message)) return;
         
         postData('./getResult.php', JSON.stringify({
             address: address,
